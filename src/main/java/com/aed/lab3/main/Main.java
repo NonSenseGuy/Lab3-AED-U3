172b4c3273f495c10c32d39717a01b0a14533008
LuisAlfredo <luisalfredo9905@gmail.com>
2018-10-18 22:02:45 -0500

    Making the Graphic Interface with some bugs

--- src/main/java/com/aed/lab3/main/Main.java
+++ src/main/java/com/aed/lab3/main/Main.java
@@ -1,8 +1,24 @@
 package com.aed.lab3.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 	
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		
+		Parent root = FXMLLoader.load(getClass().getResource("/view/window.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("FIBA");
+		primaryStage.show();
 	}
 }
