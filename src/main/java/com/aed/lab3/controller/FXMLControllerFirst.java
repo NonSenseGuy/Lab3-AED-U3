2eebb3394626a59e4bab30e5ad7be951629f8c33
LuisAlfredo <luisalfredo9905@gmail.com>
2018-10-24 20:29:39 -0500

    Method plainText in the controller of the main frame

--- src/main/java/com/aed/lab3/controller/FXMLControllerFirst.java
+++ src/main/java/com/aed/lab3/controller/FXMLControllerFirst.java
@@ -1,9 +1,13 @@
 package com.aed.lab3.controller;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import com.aed.lab3.main.Player;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -50,15 +54,17 @@ public class FXMLControllerFirst {
     private void readCSV(String path) {
     	
     	String currentLine = "";
-    	int limit = 0;
+    	int counter = 0;
     	
     	try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			
-			while((currentLine = br.readLine()) != null && limit <= 50) {
+			while((currentLine = br.readLine()) != null) {
 				String[] information = currentLine.split(",");
-				System.out.println(information[30]);
-				limit++;
+				//Player temp = new Player();
+				//plainText(counter, temp);
+				// Shots System.out.println(information[30]);
+				counter++;
 			}
 			
 			if(br != null) br.close();
@@ -66,5 +72,21 @@ public class FXMLControllerFirst {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+    }
+    
+    public void plainText(int counter, Player player) throws IOException {
+    	File file = new File( "/last/" + counter + ".txt");
+    	
+    	if(!file.exists()) file.createNewFile();
+    	
+    	FileWriter fw = new FileWriter(file);
+    	BufferedWriter writer = new BufferedWriter(fw);
+    	
+    	writer.write(player.getName() + "," + player.getTeam() + "," + player.getAge() + "," +
+    			player.getPoints() + "," + player.getRebounds() + "," + player.getAssists() + ","
+    			+ player.getSteals() + "," + player.getBlocks());
+    
+    	if(writer != null)writer.close();
+    	if(fw != null) fw.close();
     }
 }
